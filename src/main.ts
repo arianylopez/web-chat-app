d22d7f87fb98f2fdd7566b8e2ee749aaa2e5b79d
Ariany Lopez <arianylopez341@gmail.com>
2026-04-01 14:51:25 -0400

    Implementacion de login y registro con handlebars y BEM

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
 import './styles/global.scss';
+import './pages/auth/auth.scss';
+import Handlebars from 'handlebars';
+
+import loginTemplateStr from './pages/auth/login.hbs?raw';
+import registerTemplateStr from './pages/auth/register.hbs?raw';
 
 const app = document.getElementById('app');
 
-if (app) {
-  app.textContent = 'Configuración completada. Entorno listo.';
+const loginTemplate = Handlebars.compile(loginTemplateStr);
+const registerTemplate = Handlebars.compile(registerTemplateStr);
+
+function render(html: string) {
+  if (app) {
+    app.innerHTML = html;
+    attachListeners();
+  }
 }
+
+function attachListeners() {
+  const btnGoToRegister = document.getElementById('go-to-register');
+  const btnGoToLogin = document.getElementById('go-to-login');
+
+  if (btnGoToRegister) {
+    btnGoToRegister.addEventListener('click', () => render(registerTemplate({})));
+  }
+
+  if (btnGoToLogin) {
+    btnGoToLogin.addEventListener('click', () => render(loginTemplate({})));
+  }
+}
+
+render(loginTemplate({}));
